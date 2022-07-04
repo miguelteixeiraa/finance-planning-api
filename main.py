b331a42e1dfceb3339cc8ea566a64e4004faa868
Miguel Teixeira <miguel.teixeira@thoughtworks.com>
2022-07-03 21:37:31 -0300

    fixing db folder location

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from flask import Flask
-from db.MainStorageProvider import MainStorageProvider
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    db = MainStorageProvider()
-    db.deleteAll({"test": True})
-
-    return db.get("62c20ecfe8b232790b3cf1e0")
+    return "hello"
